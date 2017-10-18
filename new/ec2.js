26a4d92192243d3fdb1d29ab3c2450b8fbc6bac5
vasincaeduard <vasincaeduard@github.io>
2017-10-18 18:26:16 +0300

    am implementat mai obiectual

---
+++ new/ec2.js
@@ -0,0 +1,41 @@
+
+function ecuatie2()
+{
+//---------
+	this.citeste = function () {
+		this.a = document.getElementById("a").value;
+		this.b = document.getElementById("a").value;
+		this.c = document.getElementById("a").value;
+	};
+	
+//-----
+	this.calculeaza = function () {
+		var delta = this.b * this.b - 4 * this.a * this.c; 
+		
+		if (delta >= 0){
+		this.x1 = {re:(-this.b + Math.sqrt(delta)) / (2 * this.a), im:0}
+		this.x2 = {re:(-this.b - Math.sqrt(delta)) / (2 * this.a), im:0};
+	}
+	else{
+		this.x1 = {re:-this.b / (2 * this.a), im: Math.sqrt(-delta) / (2 * this.a)};
+		this.x2 = {re:-this.b / (2 * this.a), im:-Math.sqrt(-delta) / (2 * this.a)};
+	};
+	
+//-----
+	this.afiseaza = function () {
+		document.getElementById("x1").innerHTML = this.x1.re + "+" + this.x1.im + "i";
+		document.getElementById("x2").innerHTML = this.x2.re + "+" + this.x2.im + "i";
+	};	
+}
+}
+//-------------------------------------
+
+function rezolva()
+{
+	var e = new ecuatie2 ();
+	e.citeste();
+	e.calculeaza();
+	e.afiseaza();
+	
+}
+//-------------------------------------
