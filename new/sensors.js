854e0941feca834ad6435a1a2563fae2db787958
vasincaeduard <vasincaeduard@github.io>
2017-10-18 19:35:52 +0300

    rotunjire 2 cifre

--- new/sensors.js
+++ new/sensors.js
@@ -7,7 +7,7 @@ function ondeviceorientation(event)
  {
 	 document.getElementById("id_alpha"). innerHTML = Math.round(event.alpha * 100 ) / 100 ;
 	 document.getElementById("id_beta"). innerHTML = Math.round(event.beta * 100 ) / 100 ;
-	 document.getElementById("id_gamma"). Math.round(event.gamma * 100 ) / 100 ;
+	 document.getElementById("id_gamma"). innerHTML = Math.round(event.gamma * 100 ) / 100 ;
 
  }
  
